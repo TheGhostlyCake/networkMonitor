de1f5c231ff3ae393f75a220858a8ae66b40e310
tom <tom@tomcharter.me>
2017-12-19 21:29:04 +0000

    fixing network monitor scripts

--- networkMonitor.py
+++ networkMonitor.py
@@ -16,14 +16,14 @@ text.AddText("NetMan:", 0, 0, Id="start" )
 while(True):
 	r = requests.get(url='http://10.0.0.95/admin/api.php')
 	data=r.json()
-	text.AddText("Ads Block: " + str(data['ads_blocked_today']) +"\r\n" + "DNS queries: " + str(data['dns_queries_today)'] + "\r\n" + "Active Clients: " + str(data['unique_clients']) , 0, 20,15,  Id="main" )
-	text.AddText("time: " + strftime("%Y-%m-%d %H:%M:%S", gmtime()), 0, 65, 12, Id="time" )
-
+	text.AddText("Ads Block: " + str(data['ads_blocked_today']) +"\r\n" + "DNS queries: " + str(data['dns_queries_today']) + "\r\n" + "Active Clients: " + str(data['unique_clients']) , 0, 20,15,  Id="main" )
+	text.AddText("time: " + strftime("%Y-%m-%d %H:%M:%S", gmtime()), 0, 86, 12, Id="time" )
 	for x in range(0, 999):
 		r = requests.get(url='http://10.0.0.95/admin/api.php')
 		data=r.json()
-		text.UpdateText("main", "Ads Block: " + str(data['ads_blocked_today']) +"\r\n" + "DNS queries: " + str(data['dns_queries_today)'] + "\r\n" + "Active Clients: " + str(data['unique_clients']))
-		text.UpdateText("time", "time: " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-		time.sleep(2)
+		text.UpdateText("main", "Ads Block: " + str(data['ads_blocked_today']) +"\r\n" + "DNS queries: " + str(data['dns_queries_today']) + "\r\n" + "Active Clients: " + str(data['unique_clients']))
+		text.UpdateText("time", "time: " + strftime("%Y-%m-%d %H:%M:%S",gmtime()) )  
+		time.sleep(15)
+
 	text.clear() 
 
